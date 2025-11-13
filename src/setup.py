dac305cdd61ce83eec05bbe2b9d6b67bbef51fc0
Ramon Asuncion <asuncionbatista@gmail.com>
2025-11-12 22:49:40 -0500

    Expand setup.py to copy over templates

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,19 @@
+import os
+import shutil
 from venv import create
-from os.path import abspath
 from subprocess import run
-dir = "./tts-venv"
-create(dir, with_pip=True)
-run(["bin/pip", "install", "-r", abspath("requirements.txt")], cwd=dir)
+
+venv_dir = "./tts-venv"
+pip_path = os.path.join(venv_dir, "bin", "pip")
+
+if not os.path.isdir(venv_dir):
+    create(venv_dir, with_pip=True)
+    run([pip_path, "install", "-r", os.path.abspath("requirements.txt")], check=True)
+
+os.makedirs("private", exist_ok=True)
+
+if not os.path.isfile("private/config.yaml"):
+    shutil.copyfile("templates/config.yaml.example", "private/config.yaml")
+
+if not os.path.isfile("private/mod_blocklist.yaml"):
+    shutil.copyfile("templates/mod_blocklist.txt.example", "private/mod_blocklist.yaml")
