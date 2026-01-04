b432a2b8c4e2b21a67ef32e08397d87310f60433
Ramon Asuncion <asuncionbatista@gmail.com>
2026-01-03 22:46:40 -0500

    Add run.py

---
+++ docs/run.py
@@ -0,0 +1,8 @@
+# start http server python
+import http.server
+import socketserver
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("0.0.0.0", 8080), http.server.SimpleHTTPRequestHandler) as httpd:
+        print("Running on 0.0.0.0:8000")
+        httpd.serve_forever()
