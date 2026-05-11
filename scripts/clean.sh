e24e034a01a8b404e10ba36fc87773b2d0f181b4
Ramon Asuncion <asuncionbatista@gmail.com>
2026-05-11 12:29:47 -0400

    Remove clean.sh

--- scripts/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# This is to create new models. I have my code all of the places. Just an easy way to do this for now
-rm -rf dataset
-rm -rf third_party/TextyMcSpeechy/tts_dojo/DATASETS/custom_voice
-rm -rf third_party/TextyMcSpeechy/tts_dojo/custom_voice_dojo
